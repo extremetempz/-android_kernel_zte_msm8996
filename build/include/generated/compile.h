7ed66f0d6344ce5277fde171b59531fbadd92dfe
Prarit Bhargava <prarit@redhat.com>
2018-09-20 08:59:14 -0400

    kdb: Use strscpy with destination buffer size

--- build/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Sun Mar 3 22:12:41 +04 2019"
-#define LINUX_COMPILE_BY "baddar90"
-#define LINUX_COMPILE_HOST "hidden"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
