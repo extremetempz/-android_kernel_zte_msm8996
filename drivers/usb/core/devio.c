1e7f3f277920ed7eed70b093fb649f50800c3533
baddar90 <mnbaddar1990@gmail.com>
2019-03-03 21:38:51 +0400

    fix build error

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -1302,7 +1302,7 @@ static int proc_do_submiturb(struct usb_dev_state *ps, struct usbdevfs_urb *uurb
 				USBDEVFS_URB_BULK_CONTINUATION |
 				USBDEVFS_URB_NO_FSBR |
 				USBDEVFS_URB_ZERO_PACKET |
-				USBDEVFS_URB_NO_INTERRUPT);
+				USBDEVFS_URB_NO_INTERRUPT;
 	/* USBDEVFS_URB_ISO_ASAP is a special case */
 	if (uurb->type == USBDEVFS_URB_TYPE_ISO)
         mask |= USBDEVFS_URB_ISO_ASAP;
