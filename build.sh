d9210758c25b7b452e0246d818bccc811ff4c26c
extremetempz <justinscollo@protonmail.com>
2021-08-27 10:02:15 +1000

    fix build file

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 export KBUILD_BUILD_USER=justin
 export KBUILD_BUILD_HOST=ubuntu
 export ARCH=arm64
-export CROSS_COMPILE=$HOME/Desktop/toolchain/bin/aarch64-linux-android-
 export CROSS_COMPILE=/home/justin/android/toolchain/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
 DIR=$(pwd)
 BUILD="$DIR/build"
@@ -21,7 +20,7 @@ mkdir "$BUILD"
 make O="$BUILD" lineageos_axon7_defconfig
 
 echo "building kernel..."
-make O="$BUILD" -j30
+make O="$BUILD" -j12
 
 echo "building modules..."
 make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
